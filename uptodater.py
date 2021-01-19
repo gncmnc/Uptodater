5f717fc2f85b5f797654b04c56a32b011f880267
gncmnc <77320604+gncmnc@users.noreply.github.com>
2021-01-19 12:46:57 +0300

    Create uptodater.py

---
+++ uptodater.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+Who Contributed
+
+gncmnc <gnc@vuhuv.com>
+Samet195
+"""
+
+import requests, socket
+from requests.exceptions import ConnectionError
+from requests.exceptions import InvalidURL
+from colorama import Fore, Back, Style, init
+
+init(autoreset=True)
+
+class Uptodater():
+    version = ""
