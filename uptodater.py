153bbd4cc3fe7f48f880fcae0a61ae835b99fd79
gncmnc <77320604+gncmnc@users.noreply.github.com>
2021-01-20 09:55:22 +0300

    Update uptodater.py

--- uptodater.py
+++ uptodater.py
@@ -3,7 +3,6 @@
 
 """
 Who Contributed
-
 gncmnc <https://github.com/gncmnc>
 Samet195 <https://github.com/Samet195>
 CihatAltiparmak <http://github.com/CihatAltiparmak>
@@ -17,4 +16,23 @@ from colorama import Fore, Back, Style, init
 init(autoreset=True)
 
 class Uptodater():
-    pass
+    def github_informations(github_link, version, version_file):
+        print(Fore.LIGHTGREEN_EX  + "GitHub address : " + github_link)
+        print(Fore.LIGHTGREEN_EX + "This version : " + version)
+        print(Fore.LIGHTGREEN_EX + "Version file to search : " + version_file)
+
+    def connect(version, version_file):
+        try:
+            pass
+
+        except InvalidURL:
+            pass
+
+        except ConnectionError:
+            pass
+        
+        def compare(version, version_file):
+            pass
+
+    def uptodate():
+        pass
